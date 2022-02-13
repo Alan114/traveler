5e79da68e30ea44ba98a4124fcd6523b9a0afb13
Alan Pecenkovic <alanquad@yahoo.com>
2022-02-13 22:31:22 +0100

    conditionally showing loading screen before the data is fetched

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,12 +5,13 @@ import "./TripList.css";
 const TripList = () => {
   const [url, setUrl] = useState("http://localhost:3000/trips");
 
-  const { data: trips } = useFetch(url);
+  const { data: trips, isPending } = useFetch(url);
 
   return (
     <div className="trip-list">
       <div>
         <h2>Trip List</h2>
+        {isPending && <div>Loading trips...</div>}
         <ul>
           {trips &&
             trips.map(trip => (
